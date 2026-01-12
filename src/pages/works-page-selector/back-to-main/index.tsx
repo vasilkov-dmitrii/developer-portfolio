a248f86b4e871f31c94f12ef790dc486d1bf001b
dvasilkov <dvasilkov@accesssoftek.com>
2026-01-12 20:13:38 +0300

    deleted unused animation

--- src/pages/works-page-selector/back-to-main/index.tsx
+++ src/pages/works-page-selector/back-to-main/index.tsx
@@ -4,18 +4,6 @@ import React from "react";
 
 export const BackToMain = () => {
     return <Link to={Routes.MAIN} className="back_menu" title="Back">
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
         <div className="hidden_arrow"></div>
     </Link>
 }
