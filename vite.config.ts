299a6c1ea7b9fac02482cd3128305fcb3341f7d8
dvasilkov <dvasilkov@accesssoftek.com>
2026-01-12 19:44:11 +0300

    alias, fix node issue, package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
     plugins: [react()],
     css: {
         modules: {
